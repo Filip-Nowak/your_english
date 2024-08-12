3e6307e1218131793fe146423819545312f1bb1e
kaczka <manfil28@gmail.com>
2024-08-12 19:10:59 +0200

    changed gradle to maven

--- backend/server/src/main/java/org/example/server/security/config/RedisConfiguration.java
+++ backend/server/src/main/java/org/example/server/security/config/RedisConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 
